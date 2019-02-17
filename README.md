3e01b246a6e345f07fab971c84f730d4f263473a
Korzikowski,Yannik <yannik@korzikowski.de>
2019-02-17 19:10:59 +0100

    added flutter driver

--- README.md
+++ README.md
@@ -2,15 +2,6 @@
 
 create model releases
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.io/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.io/docs/cookbook)
-
-For help getting started with Flutter, view our 
-[online documentation](https://flutter.io/docs), which offers tutorials, 
-samples, guidance on mobile development, and a full API reference.
+## testing
+### flutter driver
+tests stored in `test_driver`dir. Can be run by command `flutter drive --target=test_driver/app.dart`
---
+++ test_driver/app_test.dart
@@ -0,0 +1,54 @@
+// Imports the Flutter Driver API
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('People Scroll', () {
+    FlutterDriver driver;
+
+    // Connect to the Flutter driver before running any tests
+    setUpAll(() async {
+      driver = await FlutterDriver.connect();
+    });
+
+    // Close the connection to the driver after the tests have completed
+    tearDownAll(() async {
+      if (driver != null) {
+        await driver.close();
+      }
+    });
+
+    test('verifies people load', () async {
+      // Create two SerializableFinders. We will use these to locate specific
+      // Widgets displayed by the app. The names provided to the byValueKey
+      // method correspond to the Keys we provided to our Widgets in step 1.
+      final listFinder = find.byValueKey('long_list');
+      final itemFinder = find.byValueKey('item_50_text');
+
+      // go to Settings
+      var tapBar = find.byValueKey('tap_bar');
+      driver.tap(tapBar);
+
+      //TODO: stub code copied from flutter.io. 
+      await driver.scrollUntilVisible(
+        // Scroll through this list
+        listFinder,
+        // Until we find this item
+        itemFinder,
+        // In order to scroll down the list, we need to provide a negative
+        // value to dyScroll. Ensure this value is a small enough increment to
+        // scroll the item into view without potentially scrolling past it.
+        //
+        // If you need to scroll through horizontal lists, provide a dxScroll
+        // argument instead
+        dyScroll: -300.0,
+      );
+
+      // Verify the item contains the correct text
+      expect(
+        await driver.getText(itemFinder),
+        'Item 50',
+      );
+    });
+  });
+}
